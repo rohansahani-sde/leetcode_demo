63cdaab776cecb20a5a520d97118ae447737da32
rohan <sahanirohan313@gmail.com>
2026-03-31 11:26:36 +0530

    Added 0-minimum-swaps-to-group-all-1's-together-ii | 0 ms | 10 MB

--- 0-minimum-swaps-to-group-all-1's-together-ii/0-minimum-swaps-to-group-all-1's-together-ii.java
+++ 0-minimum-swaps-to-group-all-1's-together-ii/0-minimum-swaps-to-group-all-1's-together-ii.java
@@ -1,12 +1,26 @@
+class Solution {
+    public int minSwaps(int[] nums) {
+        int n = nums.length;
+        int oneCount =0;
+        for(int x: nums)if(x == 1)oneCount++;
+        int[] nums1 = new int[n+n];
         for(int i=0; i<n; i++){
             nums1[i] = nums[i];
-        int[] nums1 = new int[n+n];
-        for(int x: nums)if(x == 1)oneCount++;
-        int oneCount =0;
-        int n = nums.length;
-    public int minSwaps(int[] nums) {
-class Solution {
             nums1[i+n] = nums[i]; 
         }
         int k=0;
         for(int i=0;i<oneCount; i++){
+            if(nums1[i] ==1) k++;
+        }
+        if(oneCount - k ==0) return 0;
+        int min= oneCount - k;
+        int l=0;
+        for(int i=oneCount; i<n+n; i++){
+            if(nums1[l++] ==1 ) k--;
+            if(nums1[i] == 1) k++;
+            min = Math.min(min, oneCount - k);
+        }
+        return min;
+        
+    }
+}
